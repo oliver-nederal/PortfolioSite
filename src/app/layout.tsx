4ac771348c47b0e5dd3beeddfd1019e312258974
Oliver Nederal <oliver.nederal@gmail.com>
2024-12-21 21:46:42 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import ThemeProvider from "../components/themeprovider";
 import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 export const metadata: Metadata = {
   title: "Oliver Nederal",
